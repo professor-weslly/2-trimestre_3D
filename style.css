790e7440d7345cdb6a55249bd927f4aa2c7f9911
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-05-28 07:31:06 -0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,17 @@
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
 
