ce134c1a16d96d32072e6bfcb22613703ce39cc6
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-06-16 20:23:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -56,15 +56,10 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa/*.texto*/;
-       /*botaoAlternativas.addEventListener("click", function () { */
-            /*atual++;*/
-            /*mostraPergunta();*/
-    /*})*/
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
